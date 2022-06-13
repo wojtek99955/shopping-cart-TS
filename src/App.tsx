7fccce5390813a3e8f80126099d872b65331d18f
Wojtek Książek <wojtek99955@gmail.com>
2022-06-13 12:38:45 +0200

    header added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { GlobalStyle } from "./assets/globalStyle";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <GlobalStyle />
+      <Header />
     </>
   );
 }
