e845745123c20d5cefcbc61a7833ef99f6f73b18
Wojtek Książek <wojtek99955@gmail.com>
2022-06-13 12:59:41 +0200

    ProductsList added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { GlobalStyle } from "./assets/globalStyle";
 import Header from "./components/Header";
+import { ProductsList } from "./components/ProductsList";
 
 function App() {
   return (
     <>
       <GlobalStyle />
       <Header />
+      <ProductsList />
     </>
   );
 }
