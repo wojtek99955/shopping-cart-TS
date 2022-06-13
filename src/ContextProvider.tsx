d2ab9e9ee43416c135a13eba7001248d74b8cdb9
Wojtek Książek <wojtek99955@gmail.com>
2022-06-13 19:02:16 +0200

    bug fixred

--- src/ContextProvider.tsx
+++ src/ContextProvider.tsx
@@ -4,7 +4,7 @@ type ContextType = {
   children: React.ReactNode;
 };
 
-type Products = {
+export type Products = {
   category: string;
   title: string;
   description: string;
@@ -12,6 +12,7 @@ type Products = {
   image: string;
   price: number;
   rating: Object;
+  amount: number;
 };
 
 type openCartType = {
@@ -19,15 +20,25 @@ type openCartType = {
   setOpenCart: React.Dispatch<React.SetStateAction<boolean>>;
   productsList: Products[] | null;
   setProductsList: React.Dispatch<React.SetStateAction<Products[] | null>>;
+  cartList: Products[];
+  setCartList: React.Dispatch<React.SetStateAction<Products[]>>;
 };
 
 export const Context = createContext<openCartType | null>(null);
 export const ContextProvider = ({ children }: ContextType) => {
   const [openCart, setOpenCart] = useState<boolean>(false);
   const [productsList, setProductsList] = useState<Products[] | null>(null);
+  const [cartList, setCartList] = useState<Array<Products>>([]);
   return (
     <Context.Provider
-      value={{ openCart, setOpenCart, productsList, setProductsList }}
+      value={{
+        openCart,
+        setOpenCart,
+        productsList,
+        setProductsList,
+        cartList,
+        setCartList,
+      }}
     >
       {children}
     </Context.Provider>
