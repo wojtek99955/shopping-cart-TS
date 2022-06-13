e37452b18d950b0a7e4a78009943280b0ec18492
Wojtek Książek <wojtek99955@gmail.com>
2022-06-13 15:46:07 +0200

    context added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { GlobalStyle } from "./assets/globalStyle";
+import { Cart } from "./components/Cart";
 import Header from "./components/Header";
 import { ProductsList } from "./components/ProductsList";
+import { ContextProvider } from "./ContextProvider";
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <Header />
-      <ProductsList />
+      <ContextProvider>
+        <Header />
+        <ProductsList />
+        <Cart />
+      </ContextProvider>
     </>
   );
 }
