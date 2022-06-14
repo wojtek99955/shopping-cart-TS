633eea19a9ec80b6158e4d5307a0ea90aa7397c2
Wojtek Książek <wojtek99955@gmail.com>
2022-06-14 13:39:45 +0200

    styles update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,14 +5,35 @@ import { useContext } from "react";
 
 const StyledHeader = styled.header`
   border-bottom: 1px solid grey;
-  display: flex;
-  justify-content: space-between;
-  padding: 10px 0;
-  align-items: center;
+  position: fixed;
+  width: 100%;
+  background-color: white;
+
+  padding: 0.6rem;
 `;
 const CartIcon = styled(AiOutlineShoppingCart)`
-  color: blue;
+  color: black;
   font-size: 30px;
+`;
+const IconContainer = styled.div`
+  position: relative;
+  cursor: pointer;
+
+  p {
+    position: absolute;
+    top: -0.5rem;
+    color: red;
+    right: -1rem;
+  }
+`;
+
+const Container = styled.div`
+  max-width: 1300px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: auto;
+  padding: 0.5rem;
 `;
 
 const Header: React.FC = () => {
@@ -21,10 +42,18 @@ const Header: React.FC = () => {
     ctx?.setOpenCart((prev) => !prev);
   };
   console.log(ctx?.openCart);
+  const allItems = ctx?.cartList.reduce((acc, obj) => {
+    return acc + obj.amount;
+  }, 0);
   return (
     <StyledHeader>
-      <div>Fancy Shop</div>
-      <CartIcon onClick={handleOpenCart} />
+      <Container>
+        <div>Fancy Shop</div>
+        <IconContainer>
+          <CartIcon onClick={handleOpenCart} />
+          <p>{allItems}</p>
+        </IconContainer>
+      </Container>
     </StyledHeader>
   );
 };
