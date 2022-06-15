256fef023f34003c2b6260c89766e2a39d37c88d
Wojtek Książek <wojtek99955@gmail.com>
2022-06-15 19:39:23 +0200

    radio buttons styled

--- src/components/Checkout/Payment.tsx
+++ src/components/Checkout/Payment.tsx
@@ -1,12 +1,58 @@
 import React from "react";
 import styled from "styled-components";
 import { Title } from "./OrderSummary";
+import { Formik, Field, Form } from "formik";
 
 const Container = styled.div`
   box-shadow: 0px 0px 24px -15px rgba(66, 68, 90, 1);
   border-radius: 15px;
   padding: 1rem;
+
+  label {
+    margin-bottom: 0.5rem;
+    display: block;
+  }
+
+  input {
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+
+    width: 1rem;
+    height: 1rem;
+    border: 1px solid grey;
+    border-radius: 50%;
+    position: relative;
+
+    vertical-align: middle;
+
+    &:checked::after {
+      content: "";
+      width: 11px;
+      height: 11px;
+      border-radius: 50%;
+      background: #00d0a9;
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      transform: translate(-50%, -50%);
+    }
+  }
 `;
+
+const FormContainer = styled.div`
+  padding-top: 1rem;
+`;
+
+const Column = styled.div``;
+
+interface formValues {
+  picked: string;
+}
+
+const initialValues: formValues = {
+  picked: "",
+};
 
 const Payment = () => {
   return (
@@ -15,6 +61,35 @@ const Payment = () => {
         <span>2</span>
         <h1>Payment Method</h1>
       </Title>
+      <FormContainer>
+        <Formik
+          initialValues={initialValues}
+          onSubmit={(values) => {
+            console.log(values);
+          }}
+        >
+          <Form>
+            <Column>
+              <label>
+                <Field type="radio" name="picked" value="Cash On Delivery" />
+                Cash On Delivery
+              </label>
+            </Column>
+            <Column>
+              <label>
+                <Field type="radio" name="picked" value="Bank Transfer" />
+                Bank Transfer
+              </label>
+            </Column>
+            <Column>
+              <label>
+                <Field type="radio" name="picked" value="Check" />
+                Check
+              </label>
+            </Column>
+          </Form>
+        </Formik>
+      </FormContainer>
     </Container>
   );
 };
