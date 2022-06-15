92e346713fa4bc9ad9abc0f14c843da9a6d98da6
Wojtek Książek <wojtek99955@gmail.com>
2022-06-16 00:40:19 +0200

    styles update

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -24,7 +24,8 @@ const Image = styled.img`
 `;
 
 const Container = styled.div`
-  border: 1px solid grey;
+  box-shadow: 0 12px 28px 0 rgba(0, 0, 0, 0.2), 0 2px 4px 0 rgba(0, 0, 0, 0.1),
+    inset 0 0 0 1px rgba(255, 255, 255, 0.5);
   border-radius: 20px;
   padding: 1rem;
 `;
