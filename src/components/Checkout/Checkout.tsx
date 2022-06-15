f98ec13076377a0cfdea9da0696cca9ced71a9f4
Wojtek Książek <wojtek99955@gmail.com>
2022-06-15 19:00:01 +0200

    shipping methods added

--- src/components/Checkout/Checkout.tsx
+++ src/components/Checkout/Checkout.tsx
@@ -2,18 +2,25 @@ import styled from "styled-components";
 import OrderSummary from "./OrderSummary";
 import CheckoutForm from "./CheckoutForm";
 import Payment from "./Payment";
+import ShippingMethod from "./ShippingMethod";
 
 const CheckoutContainer = styled.section`
   padding: 6rem 0;
   display: flex;
   justify-content: center;
+  gap: 2rem;
 `;
+
+const MiddleColumn = styled.div``;
 
 const Checkout = () => {
   return (
     <CheckoutContainer>
       <CheckoutForm />
-      <Payment />
+      <MiddleColumn>
+        <Payment />
+        <ShippingMethod />
+      </MiddleColumn>
       <OrderSummary />
     </CheckoutContainer>
   );
---
+++ src/components/Checkout/ShippingMethod.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Title } from "./OrderSummary";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 20rem;
+  box-shadow: 0px 0px 24px -15px rgba(66, 68, 90, 1);
+  border-radius: 15px;
+  padding: 1rem;
+`;
+
+const ShippingMethod = () => {
+  return (
+    <Container>
+      <Title>
+        <span>2</span>
+        <h1>Shipping Methods</h1>
+      </Title>
+    </Container>
+  );
+};
+
+export default ShippingMethod;
