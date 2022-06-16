4bdb9406c0792e1b72d5b7e8e2bc96999562f115
Wojtek Książek <wojtek99955@gmail.com>
2022-06-16 12:51:34 +0200

    styles/content update

--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -2,9 +2,55 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
 import { Products } from "../ContextProvider";
+import { Product } from "./Product";
 
 const Container = styled.div`
-  padding-top: 20rem;
+  padding-top: 8rem;
+  max-width: 800px;
+  margin: auto;
+  padding-left: 1rem;
+  padding-right: 1rem;
+
+  img {
+    width: 20rem;
+  }
+
+  h1 {
+    margin-bottom: 2rem;
+  }
+  h2 {
+    margin-bottom: 1.5rem;
+  }
+  h3 {
+    margin-bottom: 3rem;
+  }
+
+  p {
+    margin-top: 2rem;
+  }
+
+  button {
+    border: none;
+    color: white;
+    background-color: #00d0a9;
+    padding: 0.8rem 1.4rem;
+    cursor: pointer;
+
+    &:hover {
+      background-color: #008970;
+    }
+  }
+`;
+
+const DescriptionData = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const DescriptionContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  gap: 4rem;
 `;
 
 const ProductDetails = () => {
@@ -22,7 +68,16 @@ const ProductDetails = () => {
 
   return (
     <Container>
-      <p>{details?.title}</p>
+      <DescriptionContainer>
+        <img src={details?.image} alt={details?.title} />
+        <DescriptionData>
+          <h1>{details?.title}</h1>
+          <h2>$ {details?.price}</h2>
+          <h3>Free Returns</h3>
+          <button>add to card</button>
+        </DescriptionData>
+      </DescriptionContainer>
+      <p>{details?.description}</p>
     </Container>
   );
 };
