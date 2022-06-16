f782b8992032d0eb23255417531142ac47c2c346
Wojtek Książek <wojtek99955@gmail.com>
2022-06-16 17:25:21 +0200

    loading spinner added

--- src/components/ProductsList.tsx
+++ src/components/ProductsList.tsx
@@ -1,20 +1,30 @@
-import { useEffect, useContext } from "react";
+import { useEffect, useContext, useState } from "react";
 import styled from "styled-components";
 import { Product } from "./Product";
 import { Context } from "../ContextProvider";
 import { Products } from "../ContextProvider";
+import LoadingSpinner from "../assets/LoadingSpinner";
 
-const Container = styled.section`
+interface StyleProps {
+  loading: boolean;
+}
+
+const Container = styled.section<StyleProps>`
   max-width: 1000px;
   margin: auto;
   display: grid;
   gap: 2rem;
   grid-template-columns: 1fr 1fr 1fr;
   padding: 8rem 0;
+  height: ${({ loading }) => (loading ? "100vh" : null)};
+  display: ${({ loading }) => (loading ? "flex" : "grid")};
+  justify-content: center;
+  align-items: center;
 `;
 
 export const ProductsList: React.FC = () => {
   const ctx = useContext(Context);
+  const [loading, setLoading] = useState(true);
   const fetchData = async () => {
     const response = await fetch("https://fakestoreapi.com/products");
     const products = await response.json();
@@ -26,6 +36,7 @@ export const ProductsList: React.FC = () => {
         };
       })
     );
+    setLoading(false);
   };
   useEffect(() => {
     fetchData();
@@ -33,10 +44,16 @@ export const ProductsList: React.FC = () => {
   console.log(ctx?.productsList);
 
   return (
-    <Container>
-      {ctx?.productsList?.map((product) => (
-        <Product product={product} />
-      ))}
+    <Container loading={loading}>
+      {loading ? (
+        <LoadingSpinner />
+      ) : (
+        <>
+          {ctx?.productsList?.map((product) => (
+            <Product product={product} />
+          ))}
+        </>
+      )}
     </Container>
   );
 };
