45c907139da98a4ade2e10949664907b6b44a36b
Wojtek Książek <wojtek99955@gmail.com>
2022-06-16 10:51:33 +0200

    interfaces code rafactor

---
+++ src/components/Checkout/assets/interfaces/Interfaces.tsx
@@ -0,0 +1,12 @@
+export interface radioValues {
+  picked: string;
+}
+
+export interface FormValues {
+  firstName: string;
+  lastName: string;
+  address: string;
+  zipp: string;
+  city: string;
+  country: string;
+}
