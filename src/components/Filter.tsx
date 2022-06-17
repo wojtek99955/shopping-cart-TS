b21f1fe747e55a226ba46e4cbbc53399c5f6e368
Wojtek Książek <wojtek99955@gmail.com>
2022-06-17 09:23:51 +0200

    styles update

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -7,12 +7,48 @@ import { Products } from "../ContextProvider";
 
 const Container = styled.div`
   padding-top: 5rem;
+
+  h3 {
+    margin-bottom: 2rem;
+  }
 `;
 const initialValues: radioValues = {
   picked: "",
 };
 
-const Row = styled.div``;
+const Row = styled.div`
+  margin-bottom: 0.5rem;
+`;
+const StyledRadio = styled(Field)`
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+
+  width: 1rem;
+  height: 1rem;
+  border: 2px solid grey;
+  border-radius: 50%;
+  position: relative;
+  margin-right: 0.5rem;
+  display: inline-block;
+  vertical-align: middle;
+
+  &:hover {
+    background-color: #ccfff5;
+  }
+
+  &:checked::after {
+    content: "";
+    width: 11px;
+    height: 11px;
+    border-radius: 50%;
+    background-color: #008970;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+  }
+`;
 
 const Filter = () => {
   const [category, setCategory] = useState<null | string>(null);
@@ -53,7 +89,7 @@ const Filter = () => {
           <>
             <Row>
               <label>
-                <Field
+                <StyledRadio
                   type="radio"
                   name="picked"
                   value="electronics"
@@ -67,7 +103,7 @@ const Filter = () => {
             </Row>
             <Row>
               <label>
-                <Field
+                <StyledRadio
                   type="radio"
                   name="picked"
                   value="women's%20clothing"
@@ -81,7 +117,7 @@ const Filter = () => {
             </Row>
             <Row>
               <label>
-                <Field
+                <StyledRadio
                   type="radio"
                   name="picked"
                   value="men's%20clothing"
@@ -95,7 +131,7 @@ const Filter = () => {
             </Row>
             <Row>
               <label>
-                <Field
+                <StyledRadio
                   type="radio"
                   name="picked"
                   value="jewelery"
