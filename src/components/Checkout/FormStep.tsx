6cdfda1f95c9382e7fa2a455f47ed60bae476e0a
Wojtek Książek <wojtek99955@gmail.com>
2022-06-20 19:20:23 +0200

    form step added

---
+++ src/components/Checkout/FormStep.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import CheckoutForm from "./CheckoutForm";
+import OrderSummary from "./OrderSummary";
+import Payment from "./Payment";
+import ShippingMethod from "./ShippingMethod";
+import styled from "styled-components";
+
+interface Props {
+  step: number;
+  setStep: React.Dispatch<React.SetStateAction<number>>;
+}
+
+const Container = styled.div`
+  padding-top: 8rem;
+  max-width: 20rem;
+  margin: auto;
+`;
+
+const FormStep = ({ step, setStep }: Props) => {
+  return (
+    <Container>
+      {step === 1 && <CheckoutForm step={step} setStep={setStep} />}
+      {step === 2 && <Payment step={step} setStep={setStep} />}
+      {step === 3 && <ShippingMethod step={step} setStep={setStep} />}
+      {step === 4 && <OrderSummary />}
+    </Container>
+  );
+};
+
+export default FormStep;
