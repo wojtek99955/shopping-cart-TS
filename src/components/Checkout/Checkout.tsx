3c8d933dba33000e0e0ea6a323dc788985f1d88e
Wojtek Książek <wojtek99955@gmail.com>
2022-06-21 19:11:32 +0200

    redundant code removed

--- src/components/Checkout/Checkout.tsx
+++ src/components/Checkout/Checkout.tsx
@@ -1,8 +1,4 @@
 import styled from "styled-components";
-import OrderSummary from "./OrderSummary";
-import CheckoutForm from "./CheckoutForm";
-import Payment from "./Payment";
-import ShippingMethod from "./ShippingMethod";
 import { device } from "../../assets/media";
 import { useState } from "react";
 import FormStep from "./FormStep";
@@ -14,23 +10,6 @@ const Wrapper = styled.section`
   @media ${device.laptop} {
     max-width: 1100px;
   }
-`;
-
-const CheckoutContainer = styled.div`
-  padding: 6rem 1rem;
-  display: grid;
-  grid-template-columns: 1fr;
-  gap: 1rem;
-
-  @media ${device.laptop} {
-    grid-template-columns: repeat(3, 1fr);
-  }
-`;
-
-const MiddleColumn = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 2rem;
 `;
 
 const Checkout = () => {
