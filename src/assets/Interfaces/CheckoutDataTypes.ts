ce9461212a8a950597465ded33d4a2db737d3c94
Wojtek Książek <wojtek99955@gmail.com>
2022-06-26 09:36:19 +0200

    interface refactor

---
+++ src/assets/Interfaces/CheckoutDataTypes.ts
@@ -0,0 +1,12 @@
+export interface CheckoutDataTypes {
+  email?: string;
+  fitstName?: string;
+  lastName?: string;
+  address?: string;
+  zip?: string;
+  city?: string;
+  country?: string;
+  newsletter?: boolean;
+  payment?: string;
+  shipping?: string;
+}
