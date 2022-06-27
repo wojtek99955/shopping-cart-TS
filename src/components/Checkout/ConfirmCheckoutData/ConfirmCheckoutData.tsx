885bac835d5e22af97a3cde702909f826c9f05a2
Wojtek Książek <wojtek99955@gmail.com>
2022-06-27 13:58:37 +0200

    content update

--- src/components/Checkout/ConfirmCheckoutData/ConfirmCheckoutData.tsx
+++ src/components/Checkout/ConfirmCheckoutData/ConfirmCheckoutData.tsx
@@ -1,7 +1,48 @@
-import React from "react";
+import styled from "styled-components";
+import { CheckoutDataTypes } from "../assets/interfaces/Interfaces";
+type Props = {
+  checkoutData: CheckoutDataTypes | undefined;
+};
+const Container = styled.div`
+  h2 {
+    text-align: center;
+  }
+`;
 
-const ConfirmCheckoutData = () => {
-  return <div>ConfirmCheckoutData</div>;
+const Data = styled.div``;
+const UserData = styled.div``;
+const Address = styled.div``;
+const Shipping = styled.div``;
+const Payment = styled.div``;
+
+const ConfirmCheckoutData = ({ checkoutData }: Props) => {
+  return (
+    <Container>
+      <h2>Confirm Data</h2>
+      <Data>
+        <UserData>
+          <h3>Clients Data</h3>
+          <p>Name: {checkoutData?.fitstName}</p>
+          <p>Surname: {checkoutData?.lastName}</p>
+        </UserData>
+        <Address>
+          <h3>Shipping Address</h3>
+          <p>{checkoutData?.address}</p>
+          <p>{checkoutData?.zip}</p>
+          <p>{checkoutData?.city}</p>
+          <p>{checkoutData?.country}</p>
+        </Address>
+        <Shipping>
+          <h3>Shipping Method</h3>
+          <p>{checkoutData?.shipping}</p>
+        </Shipping>
+        <Payment>
+          <h3>Payment method</h3>
+          <p>{checkoutData?.payment}</p>
+        </Payment>
+      </Data>
+    </Container>
+  );
 };
 
 export default ConfirmCheckoutData;
