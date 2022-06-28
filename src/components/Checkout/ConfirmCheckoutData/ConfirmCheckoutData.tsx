c22a27d9e89e6d2085b35f8ff8275802a0676ed6
Wojtek Książek <wojtek99955@gmail.com>
2022-06-28 10:22:56 +0200

    code refactor

--- src/components/Checkout/ConfirmCheckoutData/ConfirmCheckoutData.tsx
+++ src/components/Checkout/ConfirmCheckoutData/ConfirmCheckoutData.tsx
@@ -1,126 +1,29 @@
-import styled from "styled-components";
 import { CheckoutDataTypes } from "../assets/interfaces/Interfaces";
 import { BtnsContainer } from "../assets/atoms/CardsStyles";
-import { device } from "../../../assets/media";
 import { Context } from "../../../ContextProvider";
 import { useContext } from "react";
 import { Link } from "react-router-dom";
+import {
+  DataContainer,
+  Data,
+  UserData,
+  Address,
+  Shipping,
+  Payment,
+  Title,
+  CartItems,
+  ItemContainer,
+  ItemDescription,
+  Wrapper,
+  StyledButton,
+  StyledLink,
+  TotalCost,
+} from "./ConfirmCheckoutDataStyles";
 
 type Props = {
   checkoutData: CheckoutDataTypes | undefined;
   setStep: React.Dispatch<React.SetStateAction<number>>;
 };
-const DataContainer = styled.div`
-  box-shadow: 0px 0px 24px -15px rgba(66, 68, 90, 1);
-  border-radius: 15px;
-  padding: 1rem;
-
-  h2 {
-    text-align: center;
-    margin-bottom: 2rem;
-  }
-  h3 {
-    margin-bottom: 0.5rem;
-  }
-  p {
-    margin-bottom: 0.6rem;
-  }
-  hr {
-    margin-bottom: 1.2rem;
-  }
-`;
-
-const Data = styled.div`
-  padding-top: 2rem;
-  margin: auto;
-  padding-bottom: 3rem;
-  @media ${device.tablet} {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    row-gap: 3rem;
-    column-gap: 2rem;
-  }
-`;
-const UserData = styled.div``;
-const Address = styled.div``;
-const Shipping = styled.div``;
-const Payment = styled.div``;
-const Title = styled.h2`
-  text-align: center;
-  margin-bottom: 2rem;
-`;
-
-const CartItems = styled.div``;
-const ItemContainer = styled.div`
-  img {
-    width: 7rem;
-    height: 7rem;
-    object-fit: contain;
-  }
-  strong {
-    font-size: 1.2rem;
-  }
-`;
-
-const ItemDescription = styled.div`
-  display: flex;
-  flex-direction: column;
-
-  span {
-    font-size: 1rem;
-    &:first-child {
-      font-weight: 600;
-      font-size: 1.2rem;
-    }
-  }
-  p {
-    font-size: 1rem;
-  }
-`;
-
-const Wrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 1rem 0;
-`;
-
-const StyledButton = styled.button`
-  background-color: #00d0a9;
-  border: none;
-  border-radius: 15px;
-  padding: 1rem 3.6rem;
-  color: white;
-  cursor: pointer;
-  margin-top: 1rem;
-  &:hover {
-    background-color: #008970;
-  }
-`;
-
-const TotalCost = styled.div`
-  margin: 1rem 0;
-  display: flex;
-  flex-direction: column;
-  max-width: 18rem;
-  margin-left: auto;
-  span {
-    text-align: end;
-  }
-  div {
-    display: flex;
-    justify-content: space-between;
-    margin-bottom: 0.8rem;
-  }
-  strong {
-    font-size: 1.2rem;
-  }
-`;
-
-const StyledLink = styled(Link)`
-  text-decoration: none;
-  color: black;
-`;
 
 const ConfirmCheckoutData = ({ checkoutData, setStep }: Props) => {
   const ctx = useContext(Context);
---
+++ src/components/Checkout/ConfirmCheckoutData/ConfirmCheckoutDataStyles.js
@@ -0,0 +1,115 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { device } from "../../../assets/media";
+
+export const DataContainer = styled.div`
+  box-shadow: 0px 0px 24px -15px rgba(66, 68, 90, 1);
+  border-radius: 15px;
+  padding: 1rem;
+
+  h2 {
+    text-align: center;
+    margin-bottom: 2rem;
+  }
+  h3 {
+    margin-bottom: 0.5rem;
+  }
+  p {
+    margin-bottom: 0.6rem;
+  }
+  hr {
+    margin-bottom: 1.2rem;
+  }
+`;
+
+export const Data = styled.div`
+  padding-top: 2rem;
+  margin: auto;
+  padding-bottom: 3rem;
+  @media ${device.tablet} {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    row-gap: 3rem;
+    column-gap: 2rem;
+  }
+`;
+export const UserData = styled.div``;
+export const Address = styled.div``;
+export const Shipping = styled.div``;
+export const Payment = styled.div``;
+export const Title = styled.h2`
+  text-align: center;
+  margin-bottom: 2rem;
+`;
+
+export const CartItems = styled.div``;
+export const ItemContainer = styled.div`
+  img {
+    width: 7rem;
+    height: 7rem;
+    object-fit: contain;
+  }
+  strong {
+    font-size: 1.2rem;
+  }
+`;
+
+export const ItemDescription = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  span {
+    font-size: 1rem;
+    &:first-child {
+      font-weight: 600;
+      font-size: 1.2rem;
+    }
+  }
+  p {
+    font-size: 1rem;
+  }
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 1rem 0;
+`;
+
+export const StyledButton = styled.button`
+  background-color: #00d0a9;
+  border: none;
+  border-radius: 15px;
+  padding: 1rem 3.6rem;
+  color: white;
+  cursor: pointer;
+  margin-top: 1rem;
+  &:hover {
+    background-color: #008970;
+  }
+`;
+
+export const TotalCost = styled.div`
+  margin: 1rem 0;
+  display: flex;
+  flex-direction: column;
+  max-width: 18rem;
+  margin-left: auto;
+  span {
+    text-align: end;
+  }
+  div {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 0.8rem;
+  }
+  strong {
+    font-size: 1.2rem;
+  }
+`;
+
+export const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: black;
+`;
