83eef6e7b8f8bb35e3e728999cacce367a9539a5
Wojtek Książek <wojtek99955@gmail.com>
2022-06-30 12:02:59 +0200

    content update

--- src/components/LikedItems.tsx
+++ src/components/LikedItems.tsx
@@ -4,19 +4,32 @@ import styled from "styled-components";
 
 const Container = styled.section`
   padding-top: 5rem;
-  max-width: 1200px;
+  max-width: 800px;
   margin: auto;
 
   h1 {
     text-align: center;
+    padding: 1rem 0;
   }
 
   img {
-    width: 10rem;
+    width: 12rem;
+    height: 12rem;
+    object-fit: cover;
   }
 `;
 
-const ItemContainer = styled.div``;
+const ItemContainer = styled.div`
+  display: flex;
+  margin: 2rem 0;
+  gap: 2rem;
+`;
+
+const ItemDescription = styled.div`
+  h3 {
+    margin-bottom: 2rem;
+  }
+`;
 
 const LikedItems = () => {
   const ctx = useContext(Context);
@@ -38,6 +51,10 @@ const LikedItems = () => {
           return (
             <ItemContainer>
               <img src={item.image} alt="" />
+              <ItemDescription>
+                <h3>{item.title}</h3>
+                <p>{item.description}</p>
+              </ItemDescription>
             </ItemContainer>
           );
         })}
